76bd0b59cb60a059287ad4651694be93776a41c3
kk professional <kkpro2007@gmail.com>
2026-03-29 13:37:05 +0530

    Css

--- src/renderer/store/index.ts
+++ src/renderer/store/index.ts
@@ -1,11 +1,38 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit'
+import { configureStore, createSlice, PayloadAction } from '@reduxjs/toolkit'
 
-const dummySlice = createSlice({
-  name: 'dummy',
-  initialState: {},
-  reducers: {}
+interface AppState {
+  theme: 'dark' | 'light'
+  sidebarOpen: boolean
+  activeModel: string
+  apiKey: string
+  plan: 'free' | 'pro'
+  generationsUsed: number
+  storageUsed: number
+}
+
+const appSlice = createSlice({
+  name: 'app',
+  initialState: {
+    theme: 'dark',
+    sidebarOpen: true,
+    activeModel: 'gpt-4o',
+    apiKey: '',
+    plan: 'free',
+    generationsUsed: 27,
+    storageUsed: 3.4,
+  } as AppState,
+  reducers: {
+    setTheme: (s, a: PayloadAction<'dark'|'light'>) => { s.theme = a.payload },
+    toggleSidebar: (s) => { s.sidebarOpen = !s.sidebarOpen },
+    setSidebar: (s, a: PayloadAction<boolean>) => { s.sidebarOpen = a.payload },
+    setModel: (s, a: PayloadAction<string>) => { s.activeModel = a.payload },
+    setApiKey: (s, a: PayloadAction<string>) => { s.apiKey = a.payload },
+    incrementGenerations: (s) => { s.generationsUsed += 1 },
+  }
 })
 
-export const store = configureStore({
-  reducer: { dummy: dummySlice.reducer }
-})
+export const { setTheme, toggleSidebar, setSidebar, setModel, setApiKey, incrementGenerations } = appSlice.actions
+
+export const store = configureStore({ reducer: { app: appSlice.reducer } })
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
